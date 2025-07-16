50073d21f5619f1901ee102484d046dc208f7fde
yahia <yhegazy@users.noreply.github.com>
2025-07-16 07:08:45 -0400

    Add files via upload

---
+++ info.py
@@ -0,0 +1,8 @@
+print("Welcome to Johanna's Argument")
+print("She is fed up with her current husband's behavior.")
+print("Help her thwart his plans using her firery flail called 'The Argument'.")
+
+
+#This is just random - it will be replaced later
+name = input("What is your name?")
+profession = input("What is your profession?")
