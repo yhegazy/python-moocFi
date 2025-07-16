d1ea1cf84aa3be5f2ed9b7dcb67cbfb09f69d44b
yahia <yhegazy@users.noreply.github.com>
2025-07-16 08:12:33 -0400

    Add files via upload

---
+++ absoluteValue.py
@@ -0,0 +1,7 @@
+# Write your solution here
+number = int(input("Please type in a number:"))
+
+if number < 0:
+    number *= -1
+
+print(f"The absolute value of this number is {number}")
---
+++ seinfield.py
@@ -0,0 +1,8 @@
+# Write your solution here
+name = input("Please tell me your name:")
+
+if name != "Jerry":
+    soup = int(input("How many portions of soup?"))
+    print(f"The total cost is {soup * 5.90}")
+
+print("Next please!")
